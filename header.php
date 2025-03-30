4f4af1668a9c3c53caee0070b79192261b265b1f
Peter Kore <kore.peter@gmail.com>
2025-03-30 21:51:29 +0200

    header átszerkesztve, hogy megegyezzen a html fájllal

--- header.php
+++ header.php
@@ -7,13 +7,29 @@
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="CSS/style.css">
+<link rel="stylesheet" href="styles.css">
 
 </head>
 <!--  html kód -->
   <body>
+
 <header>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <h1>Kutya webshop</h1>    
+        <nav>
+	    <ul class="menu">
+                <li>|</li>
+		<li><a href="index.html">Kezdőlap</a></li>
+		<li>|</li>
+                <li><a href="termekek.html">Megrendelés-Termékek a kosárban</a></li>
+		<li>|</li>
+                <li><a href="#">Kapcsolat</a></li>
+		<li>|</li>
+            </ul>
+        </nav>
+
+        <div class="cart" onclick="viewCart()"> Kosár (<span id="cart-count">0</span>)</div>
+    </header>
+<!--nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -33,8 +49,7 @@
         <input class="form-control me-2" type="search" name = "keres" placeholder="Keres" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Keres</button>
       </form>
-    </div>
-  </div>
-</nav>
+    </div-->
+  <!--/div>
+</nav-->
 
-</header>
